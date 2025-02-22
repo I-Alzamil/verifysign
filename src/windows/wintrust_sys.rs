37d22bce77ce71829ae3f445537836ede2382782
I-Alzamil <ibrahim@alzamil-sa.com>
2025-02-22 23:25:34 +0300

    Update dependencies and update code to new requirements

--- src/windows/wintrust_sys.rs
+++ src/windows/wintrust_sys.rs
@@ -1,4 +1,4 @@
-use std::ffi::{c_int, c_uchar, c_ulong};
+use std::ffi::{c_uchar, c_ulong};
 
 pub use windows_sys::Win32::Foundation::{
     CloseHandle, GetLastError, ERROR_INVALID_PARAMETER, FALSE, GENERIC_READ, HANDLE,
@@ -8,7 +8,7 @@ pub use windows_sys::Win32::Security::Cryptography::Catalog::*;
 pub use windows_sys::Win32::Security::Cryptography::*;
 pub use windows_sys::Win32::Security::WinTrust::*;
 pub use windows_sys::Win32::Storage::FileSystem::{
-    CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, OPEN_EXISTING,
+    CreateFileW, FILE_SHARE_READ, OPEN_EXISTING,
 };
 pub use windows_sys::Win32::System::Threading::{
     OpenProcess, QueryFullProcessImageNameW, PROCESS_QUERY_LIMITED_INFORMATION,
@@ -17,22 +17,4 @@ pub use windows_sys::Win32::System::Threading::{
 #[allow(non_camel_case_types)]
 pub type PCCERT_CONTEXT = *const CERT_CONTEXT;
 pub type DWORD = c_ulong;
-pub type BOOL = c_int;
 pub type BYTE = c_uchar;
-
-#[link(name = "Wintrust")]
-extern "system" {
-    pub fn WTHelperProvDataFromStateData(handle: HANDLE) -> *const std::ffi::c_void;
-
-    pub fn WTHelperGetProvSignerFromChain(
-        pProvData: *const std::ffi::c_void,
-        idxSigner: DWORD,
-        fCounterSigner: BOOL,
-        idxCounterSigner: DWORD,
-    ) -> *const std::ffi::c_void;
-
-    pub fn WTHelperGetProvCertFromChain(
-        pSgnr: *const std::ffi::c_void,
-        idxCert: DWORD,
-    ) -> *const std::ffi::c_void;
-}
